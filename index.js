c653c0a93d272150c60ff02fce67058a831f9ada
tayronea <tayron_machado@hotmail.com>
2019-09-11 17:17:19 -0300

    create server node

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require ('express');
+const cors = require ('cors');
+const mysql = require('mysql');
+
+const app = express();
+
+const SELECT_ALL_USERS_QUERY = 'SELECT * FROM usuario';
+
+const connection = mysql.createConnection({
+    host: 'localhost' ,
+    user: 'root',
+    password: '',
+    database: 'pvp_game'
+});
+
+
+connection.connect(err => {
+    if (err) 
+        return err;
+})
+
+
+console.log(connection);
+
+
+app.use (cors());
+
+app.get('/', (req , res) =>{
+    res.send('Conectado')
+});
+
+
+app.get('/user', (req, res) => {
+    connection.query(SELECT_ALL_USERS_QUERY, (err, results) =>{
+
+        if(err){
+            return res.send(err)
+        }
+        else{
+            return res.json({
+                data: results
+            })
+        }
+    });
+});
+
+app.listen (4000, () => {
+    console.log(`Server on`)
+})
---
+++ pvp_game.sql
@@ -0,0 +1,9 @@
+Create table user(  
+
+    user_id (int),
+    name_user varchar(255),
+    email varchar(255),
+    password_user varchar(50),
+
+ primary key (user_id)
+)
